0eff1ca769e869b9d436551c1150ea0c192fb1cd
Ariel Martin <arielmartin80@gmail.com>
2020-02-29 19:34:08 -0300

    Se actualizó footer
    se cambió google+por whatsapp

--- view/footer/pie.php
+++ view/footer/pie.php
@@ -1,22 +1,27 @@
 <footer>   
     <div class="container_12">
       
-      <div class="grid_12">
-        <div class="socials">
-      <section id="facebook">
-        <a href="https://www.facebook.com/EvaluateOficial" target="_blank"><span  class="tooltip" title="Seguinos en Facebook">f</span></a>
-        </section>
-        <section id="twitter">
-        <a href="https://twitter.com/EvaluateOk" target="_blank"><span id="twitter-default" class="tooltip" title="Seguinos en Twitter">t</span></a>
-        </section>      
-        <section id="google">
-        <a href="https://plus.google.com/u/0/113947012024704639679/posts?hl=es" target="_blank"><span id="google-default" class="tooltip" title="Seguinos en Google +">g<span>+</span></span></a>
-        </section>    
-        </div>
-        <div class="copy">
-        <a href="Manual de usuario.pdf">Manual Del Usuario </a>| Evaluate &copy; 2014 |<a href="#">Todos los derechos reservados</a> <br>
-        </div>
+        <div class="grid_12">
+            <div class="socials">
+            
+                <section id="facebook">
+                <a href="https://www.facebook.com/EvaluateOficial" target="_blank"><span  class="tooltip" title="Seguinos en Facebook">f</span></a>
+                </section>
+
+                <section id="twitter">
+                <a href="https://twitter.com/EvaluateOk" target="_blank"><span id="twitter-default" class="tooltip" title="Seguinos en Twitter">t</span></a>
+                </section>
+
+                <section id="google">
+                <a href="https://api.whatsapp.com/send?phone=5491157673825&text=Podras colocar tu mensaje aquí" target="_blank"><span id="google-default" class="tooltip" title="Contactanos por Whatsapp"><img id="ws" src="../public/images/whatsapp.png" alt=""/></a>
+                </section>
+
+            </div>
+
+            <div class="copy">
+                <a href="Manual de usuario.pdf">Manual Del Usuario </a>| Evaluate &copy; 2015 |<a href="#">Todos los derechos reservados</a> <br>
+            </div>
          
-      </div>
+        </div>
     </div>  
 </footer>
